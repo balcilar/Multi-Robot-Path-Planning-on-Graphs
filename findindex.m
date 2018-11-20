07242f9caa9d976c5a2290c50b38a3cee1d77c21
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-11-20 23:26:10 +0100

    Update findindex.m

--- findindex.m
+++ findindex.m
@@ -3,7 +3,7 @@ function [value ind]=findindex(Cost,curr)
 % all rights reverved
 value=inf;
 ind=size(Cost,1)+1;
-I=sum( abs(Cost(:,1:end-1)-curr)');
+I=sum( abs(Cost(:,1:end-1)-repmat(curr,size(Cost,1),1))');
 i=find(I==0);
 if length(i)>0
     value=Cost(i(1),end);
